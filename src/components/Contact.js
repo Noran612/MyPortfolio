cda7da68d6363fde3d4e6d7f837235c5b0ca37e6
Noran612 <noranomar784@gmail.com>
2023-11-26 04:57:14 +0200

    Your descriptive commit message here

---
+++ src/components/Contact.js
@@ -0,0 +1,100 @@
+import { useState } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import contactImg from "../assets/img/contact-img.svg";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+import emailjs from 'emailjs-com'; // Import emailjs library
+
+export const Contact = () => {
+  const formInitialDetails = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    phone: '',
+    message: ''
+  }
+  const [formDetails, setFormDetails] = useState(formInitialDetails);
+  const [buttonText, setButtonText] = useState('Send');
+  const [status, setStatus] = useState({});
+
+  const onFormUpdate = (category, value) => {
+      setFormDetails({
+        ...formDetails,
+        [category]: value
+      })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setButtonText("Sending...");
+
+    // Set up email service using emailjs
+    emailjs.sendForm(
+      'service_tm2ds0o', // Replace with your emailjs service ID
+      'template_y614ai8', // Replace with your emailjs template ID
+      e.target, // Pass the form element
+       // Replace with your emailjs user ID
+       'xJoHKXd1hQaJwVpdf'
+    )
+    
+      .then((response) => {
+        setButtonText("Send");
+        setFormDetails(formInitialDetails);
+        setStatus({ success: true, message: 'Message sent successfully' });
+      }, (error) => {
+        setButtonText("Send");
+        setStatus({ success: false, message: 'Something went wrong, please try again later.' });
+        console.error('Error:', error);
+      });
+  };
+
+  return (
+    <section className="contact" id="connect">
+      <Container>
+        <Row className="align-items-center">
+          <Col size={12} md={6}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+                <img className={isVisible ? "animate__animated animate__zoomIn" : ""} src={contactImg} alt="Contact Us"/>
+              }
+            </TrackVisibility>
+          </Col>
+          <Col size={12} md={6}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                <h2>Get In Touch</h2>
+                <form onSubmit={handleSubmit}>
+                  <Row>
+                    <Col size={12} sm={6} className="px-1">
+                      <input type="text" name="firstName" value={formDetails.firstName} placeholder="First Name" onChange={(e) => onFormUpdate('firstName', e.target.value)} />
+                    </Col>
+                    <Col size={12} sm={6} className="px-1">
+                      <input type="text" name="lastName" value={formDetails.lasttName} placeholder="Last Name" onChange={(e) => onFormUpdate('lastName', e.target.value)}/>
+                    </Col>
+                    <Col size={12} sm={6} className="px-1">
+                      <input type="email" name="email" value={formDetails.email} placeholder="Email Address" onChange={(e) => onFormUpdate('email', e.target.value)} />
+                    </Col>
+                    <Col size={12} sm={6} className="px-1">
+                      <input type="tel" name="phone" value={formDetails.phone} placeholder="Phone No." onChange={(e) => onFormUpdate('phone', e.target.value)}/>
+                    </Col>
+                    <Col size={12} className="px-1">
+                      <textarea name="message" rows="6" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
+                      <button type="submit"><span>{buttonText}</span></button>
+                    </Col>
+                    {
+                      status.message &&
+                      <Col>
+                        <p className={status.success === false ? "danger" : "success"}>{status.message}</p>
+                      </Col>
+                    }
+                  </Row>
+                </form>
+              </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  )
+}
---
+++ src/components/Footer.js
@@ -0,0 +1,27 @@
+import { Container, Row, Col } from "react-bootstrap";
+import { MailchimpForm } from "./MailchimpForm";
+
+import navIcon1 from "../assets/img/nav-icon1.svg";
+import navIcon2 from "../assets/img/nav-icon2.svg";
+import navIcon3 from "../assets/img/nav-icon3.svg";
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <Container>
+        <Row className="align-items-center">
+          <MailchimpForm />
+         
+          <Col size={12} sm={6} className="text-center text-sm-end">
+            <div className="social-icon">
+              <a href="https://www.linkedin.com/in/noran-omar-muhammed-7bb17620b/"><img src={navIcon1} alt="Icon" /></a>
+              <a href="https://github.com/Noran612"><img src={navIcon2} alt="Icon" /></a>
+              <a href="mailto:noranomar784@gmail.com"><img src={navIcon3} alt="Icon" /></a>
+            </div>
+            <p>Thank you for your time!</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  )
+}
